0d922727a307dc54ae7b27d38f1578d190032e6c
mightoz <oscar.beronius@gmail.com>
2015-01-29 20:24:47 +0100

    started working on button

--- src/gui/Button.java
+++ src/gui/Button.java
@@ -2,26 +2,48 @@ package gui;
 
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+
 
 public class Button extends GuiEntity{
 
 	private Mouse mouse;
+	private Image button;
+	private int height;
+	private int width;
+	private boolean buttonClicked;
+	private boolean buttonPressed;
 	
-	public Button(int xPos, int yPos, Mouse mouse) {
+	
+	public Button(int xPos, int yPos, Mouse mouse, Image image, int width, int height) {
 		super(xPos, yPos);
-		mouse = mouse;
+		buttonClicked = false;
+		buttonPressed = false;
+		
+		button = image;
+		this.mouse = mouse;
+		this.width = width;
+		this.height = height;
 	}
 
-	@Override
+	
 	public void draw(Graphics g) {
-		
-		
+		g.drawImage(button, getxPos(), getyPos());	
 	}
 
 	@Override
 	public void update(int delta) {
-		// TODO Auto-generated method stub
 		
+		if(!mouse.isButtonDown(0) && buttonPressed && (mouse.getX() > getxPos() && mouse.getX() <= getxPos() + width && mouse.getY() > getyPos() && mouse.getY() <= getyPos() + height)){
+			buttonClicked = true;
+		}
+		
+		if(mouse.isButtonDown(0) && (mouse.getX() > getxPos() && mouse.getX() <= getxPos() + width && mouse.getY() > getyPos() && mouse.getY() <= getyPos() + height)){
+			buttonPressed = true;
+		}else{
+			buttonPressed = false;
+		}
+			
 	}
 
 }
